0ad8d4ffb151805d201d2a8a5083171f551dc247
Vinay <itsvinayadav@gmail.com>
2022-07-18 21:57:07 +0530

    Fixed old  accordion close while opening new one

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -22,7 +22,7 @@ const Faq = () => {
                                     What is your work dicipline?
                                 </button>
                             </h2>
-                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne" data-bs-parent="#accordionPanelsStayOpenExample">
                                 <div class="accordion-body">
                                     <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                                 </div>
@@ -34,7 +34,7 @@ const Faq = () => {
                                     What is your location & timezone?
                                 </button>
                             </h2>
-                            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo" data-bs-parent="#accordionPanelsStayOpenExample">
                                 <div class="accordion-body">
                                     <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                                 </div>
@@ -46,7 +46,7 @@ const Faq = () => {
                                     How often can we communicate?
                                 </button>
                             </h2>
-                            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree" data-bs-parent="#accordionPanelsStayOpenExample">
                                 <div class="accordion-body">
                                     <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                                 </div>
@@ -58,7 +58,7 @@ const Faq = () => {
                                     What is your hourly rate?
                                 </button>
                             </h2>
-                            <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
+                            <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour" data-bs-parent="#accordionPanelsStayOpenExample">
                                 <div class="accordion-body">
                                     <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                                 </div>
--- src/components/Worked.js
+++ src/components/Worked.js
@@ -3,47 +3,6 @@ import './Worked.css';
 
 const Worked = () => {
   return (
-    // <section id="worked-with" className="mt-5 mb-5">
-    //   <div className='container'>
-    //     <div className='main-content wow fadeInUp'>
-    //       <h2>I Worked With</h2>
-    //       <div className='worked-carousel slick-initialized slick-slider'>
-    //         <div className='slick-list draggable'>
-    //           <div className='slick-track d-flex justify-content-center'>
-    //             <div className='slick-slide text-center' style={{ width: '200px' }}>
-    //               <div>
-    //                 <div className='single-item py-3'>
-    //                   <img src="/brand-1.png" alt="img" />
-    //                 </div>
-    //               </div>
-    //             </div>
-    //             <div className='slick-slide text-center' style={{ width: '200px' }}>
-    //               <div>
-    //                 <div className='single-item py-3'>
-    //                   <img src="/brand-2.png" alt="img" />
-    //                 </div>
-    //               </div>
-    //             </div>
-    //             <div className='slick-slide text-center' style={{ width: '200px' }}>
-    //               <div>
-    //                 <div className='single-item py-3'>
-    //                   <img src="/brand-3.png" alt="img" />
-    //                 </div>
-    //               </div>
-    //             </div>
-    //             <div className='slick-slide text-center' style={{ width: '200px' }}>
-    //               <div>
-    //                 <div className='single-item py-3'>
-    //                   <img src="/brand-4.png" alt="img" />
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
 
     <section id="worked-with" className="mt-5 mb-5">
       <div className='container'>
