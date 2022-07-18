ff21cb03d03211770b3cbb80a80f069fa61369a1
Vikram Singh <85120364+vikramsingh159@users.noreply.github.com>
2022-07-18 16:25:56 +0530

    scrollToTop active to work and funtion to work scroll top chnages js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './ScrollToTop.css';
 
-function ScrollToTop() {
+const ScrollButton = () => {
+
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true)
+    }
+    else if (scrolled <= 300) {
+      setVisible(false)
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
   return (
-    <div>
-      <a href="/" className="scrollToTop active"><i className="fas fa-angle-double-up"></i></a>
-    </div>
+    <button className="scrollToTop active bg-light" onClick={scrollToTop} style={{ display: visible ? 'inline' : 'none' }} >
+      <i class="fas fa-angle-double-up"></i>
+    </button>
+
   )
 }
 
-export default ScrollToTop
+export default ScrollButton;
